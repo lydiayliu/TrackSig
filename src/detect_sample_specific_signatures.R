33a9ab9f80efdd9fc87db4379b8578edda912cc9
foreverwander <lydia.liu@mail.utoronto.ca>
2021-08-31 18:01:37 -0400

    write overall exposures to file

--- src/detect_sample_specific_signatures.R
+++ src/detect_sample_specific_signatures.R
@@ -1,9 +1,14 @@
 ## LOAD
 simulated_data <- FALSE
+cancer_type_signatures <- TRUE
+compute_bootstrap <- FALSE
+
 source("src/header.R")
 
 ## DATA
 exposures <- compute_overall_exposures_for_all_examples()
+
+write.table(exposures, '/output/sample_overall_exposures.txt', sep='\t', quote=F, row.names=F, col.names=T)
 
 # 5% threshold
 exposures[exposures < 0.05] <- 0
